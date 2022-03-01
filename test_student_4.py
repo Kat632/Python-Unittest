0b24f689cd71f5d0f2412426e0d5226c8f6a9b46
Kat632 <katherine.biggs@mail.com>
2022-03-01 14:50:39 +0000

    use of setUp and tearDown methods

---
+++ test_student_4.py
@@ -0,0 +1,31 @@
+import unittest
+from student_4 import Student
+
+
+class TestStudent(unittest.TestCase):
+
+    def setUp(self):
+        print('set up')
+        self.student = Student('Griffin', 'Morley-Biggs')
+    
+    def tearDown(self):
+        print('tear down')
+    
+    def test_full_name(self):
+        print('test full name')
+
+        self.assertEqual(self.student.full_name, 'Griffin Morley-Biggs')
+    
+    def test_alert_santa(self):
+        print('test alert santa')
+        self.student.alert_santa()
+
+        self.assertTrue(self.student.naughty_list)
+    
+    def test_email(self):
+        print('test email')
+        self.assertEqual(self.student.email, 'griffin.morley-biggs@email.com')
+
+
+if __name__ == '__main__':
+    unittest.main()
